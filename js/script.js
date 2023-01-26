26b655b3993f1186a22af67bee377fb3b62ba30d
Vlad <bfg7@mail.ru>
2023-01-26 13:31:46 +0300

    Added my project

---
+++ js/script.js
@@ -0,0 +1,62 @@
+//Модальное окно
+
+const link = document.querySelector(".send-us-form-button");
+const popup = document.querySelector(".modal");
+const close = document.querySelector(".modal-close");
+const login = popup.querySelector("[name=send-us-name]");
+const email = popup.querySelector("[name=send-us-email]");
+const fieldText = popup.querySelector("[name=send-us-text]");
+const form = popup.querySelector("form");
+const storage = localStorage.getItem("login");
+
+let isStorageSupport = true;
+
+try {
+  storage = localStorage.getItem("login");
+  }
+catch (err) {
+  isStorageSupport = false;
+  }
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    console.log("Клик по кнопке Напишите нам");
+    popup.classList.add("modal-show");
+    console.log("Открывается модалка");
+    login.focus();
+    if (storage) {
+      login.value = storage;
+      email.focus();
+    }
+    else {
+      login.focus();
+    }
+ });
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    console.log("Закрывается модалка");
+    popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!login.value || !email.value || !fieldText.value) {
+    evt.preventDefault();
+    console.log("Нужно ввести логин и пароль");
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");}
+  else {
+    localStorage.setItem("login", login.value);}
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.key === "Escape") {
+    if (popup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+      }
+    }
+  });
